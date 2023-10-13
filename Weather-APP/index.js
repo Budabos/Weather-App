2a6a5fd695657e2211fb8a84322713e3b2921929
caroline opiyo <opiyocaroline20@gmail.com>
2023-10-13 06:06:38 +0300

    Check the weather condition and set the weather icon accordingly

--- Weather-APP/index.js
+++ Weather-APP/index.js
@@ -22,7 +22,7 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 // Define an asynchronous function to check the weather
-async function checkWeather(city) {
+/*async function checkWeather(city) {
     // Send a GET request to the OpenWeatherMap API with your API key
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
@@ -32,6 +32,7 @@ async function checkWeather(city) {
     // Log the weather data to the console
     console.log(data);
 
+
     
     // Update HTML elements with weather data received from the API response
     document.querySelector(".city").innerHTML = data.name;
@@ -40,32 +41,81 @@ async function checkWeather(city) {
     document.querySelector(".wind").innerHTML = data.main.wind.speed + " km/h"; 
 
 
-    if(data.weather[0].main ==="Clouds"){
+
+    if(data.weather[0].main =="Clouds"){
       weatherIcon.src = "images/clouds.png";
     }
-    else if(data.weather[0].main ==="Clear"){
+    else if(data.weather[0].main =="Clear"){
       weatherIcon.src = "images/clear.png"; 
     }
-    else if(data.weather[0].main ==="Rain"){
+    else if(data.weather[0].main =="Rain"){
       weatherIcon.src = "images/rain.png"; 
     }
-    else if(data.weather[0].main ==="Drizzle"){
+    else if(data.weather[0].main =="Drizzle"){
       weatherIcon.src = "images/drizzle.png"; 
     }
-    else if(data.weather[0].main ==="Mist"){
+    else if(data.weather[0].main =="Mist"){
       weatherIcon.src = "images/mist.png"; 
     }
-    else if(data.weather[0].main ==="Snow"){
+    else if(data.weather[0].main =="Snow"){
       weatherIcon.src = "images/snow.png"; 
     }
-    else if(data.weather[0].main ==="Wind"){
-      weatherIcon.src = "images/wind.png"; 
+    else if(data.weather[0].main =="Wind"){
+      weatherIcon.src = "images/rain.png"; 
     }
+
+    
+    
 
     // Create a mapping of weather conditions to image sources
 
 
     
+}*/
+
+// Define an asynchronous function to check the weather
+async function checkWeather(city) {
+  try {
+      // Send a GET request to the OpenWeatherMap API with your API key
+      const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+      // Check if the response status is okay (200)
+      if (response.status === 200) {
+          // Parse the JSON response from the API
+          let data = await response.json();
+
+          // Log the weather data to the console
+          console.log(data);
+
+          // Update HTML elements with weather data received from the API response
+          document.querySelector(".city").innerHTML = data.name;
+          document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"; 
+          document.querySelector(".humidity").innerHTML = data.main.humidity + "%"; 
+          document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"; 
+
+          // Check the weather condition and set the weather icon accordingly
+          if (data.weather[0].main == "Clouds") {
+              weatherIcon.src = "images/clouds.png";
+          } else if (data.weather[0].main == "Clear") {
+              weatherIcon.src = "images/clear.png"; 
+          } else if (data.weather[0].main == "Rain") {
+              weatherIcon.src = "images/rain.png"; 
+          }else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png"; 
+          }else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png"; 
+          }else if (data.weather[0].main == "wind") {
+            weatherIcon.src = "images/wind.png"; 
+          }else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+          } 
+        
+      } else {
+          console.error("Response status is not 200.");
+      }
+  } catch (error) {
+      console.error("An error occurred:", error);
+  }
 }
 
 searchBtn.addEventListener("click", ()=>{
