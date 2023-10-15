48585d884a152b44cc44182f1b0c8f37fc28d2c4
caroline opiyo <opiyocaroline20@gmail.com>
2023-10-15 22:29:38 +0300

    adding eventlistener

--- Weather-APP/index.js
+++ Weather-APP/index.js
@@ -21,57 +21,6 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");  
 const weatherIcon = document.querySelector(".weather-icon");
 
-// Define an asynchronous function to check the weather
-/*async function checkWeather(city) {
-    // Send a GET request to the OpenWeatherMap API with your API key
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
-    // Parse the JSON response from the API
-    let data = await response.json();
-
-    // Log the weather data to the console
-    console.log(data);
-
-
-    
-    // Update HTML elements with weather data received from the API response
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"; 
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"; 
-    document.querySelector(".wind").innerHTML = data.main.wind.speed + " km/h"; 
-
-
-
-    if(data.weather[0].main =="Clouds"){
-      weatherIcon.src = "images/clouds.png";
-    }
-    else if(data.weather[0].main =="Clear"){
-      weatherIcon.src = "images/clear.png"; 
-    }
-    else if(data.weather[0].main =="Rain"){
-      weatherIcon.src = "images/rain.png"; 
-    }
-    else if(data.weather[0].main =="Drizzle"){
-      weatherIcon.src = "images/drizzle.png"; 
-    }
-    else if(data.weather[0].main =="Mist"){
-      weatherIcon.src = "images/mist.png"; 
-    }
-    else if(data.weather[0].main =="Snow"){
-      weatherIcon.src = "images/snow.png"; 
-    }
-    else if(data.weather[0].main =="Wind"){
-      weatherIcon.src = "images/rain.png"; 
-    }
-
-    
-    
-
-    // Create a mapping of weather conditions to image sources
-
-
-    
-}*/
 
 // Define an asynchronous function to check the weather
 async function checkWeather(city) {
@@ -85,7 +34,7 @@ async function checkWeather(city) {
           let data = await response.json();
 
           // Log the weather data to the console
-          console.log(data);
+  
 
           // Update HTML elements with weather data received from the API response
           document.querySelector(".city").innerHTML = data.name;
@@ -116,6 +65,8 @@ async function checkWeather(city) {
   } catch (error) {
       console.error("An error occurred:", error);
   }
+
+  document.querySelector(".weather").style.display = "block"
 }
 
 searchBtn.addEventListener("click", ()=>{
